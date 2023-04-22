9c1c2cdec911c64e165ffeee6adb318f9bc3d2e2
Matthew Rathkey <rathkeym@gmail.com>
2023-04-22 15:26:55 -0600

    Create Passenger model class

--- flights/models.py
+++ flights/models.py
@@ -17,3 +17,11 @@ class Flight(models.Model):
 
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
+
+class Passenger(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
