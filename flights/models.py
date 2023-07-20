a2099e76787490dab35853428c51e22c82697148
Matthew Rathkey <rathkeym@gmail.com>
2023-07-20 02:04:23 -0600

    Add is_valid_flight()

--- flights/models.py
+++ flights/models.py
@@ -18,6 +18,9 @@ class Flight(models.Model):
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
 
+    def is_valid_flight(self):
+        return self.origin != self.destination and self.duration > 0
+
 class Passenger(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
