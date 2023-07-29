0bb7b9c928a3a3014b9ae69a970033bb2993cf50
Matthew Rathkey <rathkeym@gmail.com>
2023-07-29 01:16:20 -0600

    Begin adding dummy data

--- flights/tests.py
+++ flights/tests.py
@@ -4,3 +4,21 @@ from django.test import Client, TestCase
 from .models import Airport, Flight, Passenger
 
 # Create your tests here.
+class FlightTestCase(TestCase):
+
+    # Create dummy data for testing
+    def setUp(self):
+
+        # Dummy airports
+        a1 = Airport.objects.create(code="AAA", city="City A")
+        a2 = Airport.objects.create(code="BBB", city="City B")
+
+        # Dummy flights
+        Flight.objects.create(origin=a1, destination=a2, duration=100)
+        Flight.objects.create(origin=a1, destination=a1, duration=200)
+        Flight.objects.create(origin=a1, destination=a2, duration=-100)
+
+    # Create various tests
+    def test_departures_count(self):
+        a = Airport.objects.get(code="AAA")
+        self.assertEqual(a.departures.count(), 3)
