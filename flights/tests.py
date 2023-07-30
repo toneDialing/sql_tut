e228bcd188ba2442d57652480389054b11a2b05b
Matthew Rathkey <rathkeym@gmail.com>
2023-07-30 17:48:59 -0600

    Add test_arrivals_count(), test_valid_flight()

--- flights/tests.py
+++ flights/tests.py
@@ -22,3 +22,13 @@ class FlightTestCase(TestCase):
     def test_departures_count(self):
         a = Airport.objects.get(code="AAA")
         self.assertEqual(a.departures.count(), 3)
+
+    def test_arrivals_count(self):
+        a = Airport.objects.get(code="AAA")
+        self.assertEqual(a.arrivals.count(), 1)
+
+    def test_valid_flight(self):
+        a1 = Airport.objects.get(code="AAA")
+        a2 = Airport.objects.get(code="BBB")
+        f = Flight.objects.get(origin=a1, destination=a2, duration=100)
+        self.assertTrue(f.is_valid_flight())
