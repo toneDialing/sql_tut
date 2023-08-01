541f19e7ed892b380894917067d74394c533c6e7
Matthew Rathkey <rathkeym@gmail.com>
2023-08-01 00:27:17 -0600

    Add test_flight_page_passengers()

--- flights/tests.py
+++ flights/tests.py
@@ -69,4 +69,14 @@ class FlightTestCase(TestCase):
         response = c.get(f"/flights/{max_id + 1}")
         self.assertEqual(response.status_code, 404)
 
+    def test_flight_page_passengers(self):
+        f = Flight.objects.get(pk=1)
+        p = Passenger.objects.create(first="Alice", last="Adams")
+        f.passengers.add(p)
+
+        c = Client()
+        response = c.get(f"/flights/{f.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["passengers"].count(), 1)
+
 
